6b5765ad6b8b9d77bf084e1c147e243015d33c46
Marcelo dos Santos <marcelinho.cost@gmail.com>
2024-07-24 13:14:22 -0300

    Created Login Page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,50 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { FormEvent, useState } from "react";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log(email, password);
+  }
+
   return (
-    <div>
-      <h1>Pagina Login</h1>
+    <div className="flex w-full h-screen items-center justify-center flex-col">
+      <Link to="/">
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
+          Dev
+          <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
+            Link
+          </span>
+        </h1>
+      </Link>
+      <form
+        onSubmit={handleSubmit}
+        action=""
+        className="px-1 w-full max-w-xl flex flex-col"
+      >
+        <Input
+          placeholder="Digite o seu email..."
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="***********"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white"
+        >
+          Acessar
+        </button>
+      </form>
     </div>
   );
 }
