df0b5a67e426d2d28ff1051fd31739094e52d5db
Marcelo dos Santos <marcelinho.cost@gmail.com>
2024-07-24 12:58:40 -0300

    Created Social Components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,32 @@
+import { Social } from "../../components/Social";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export function Home() {
   return (
-    <div className="w-" >
-      <h1>Pagina Home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
+        Sujeito Progamador
+      </h1>
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a href="">
+            <p className="md:text-lg text-base">Canal no Youtube</p>
+          </a>
+        </section>
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url="https://facebook.com/sujeitoprogramador">
+            <FaFacebook size={35} color="#fff" />
+          </Social>
+          <Social url="https://youtube.com/sujeitoprogramador">
+            <FaYoutube size={35} color="#fff" />
+          </Social>
+          <Social url="https://instagram.com/sujeitoprogramador">
+            <FaInstagram size={35} color="#fff" />
+          </Social>
+        </footer>
+      </main>
     </div>
   );
 }
